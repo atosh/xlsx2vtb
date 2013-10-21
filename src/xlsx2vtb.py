d58c10ac832c5700558dfb5acf11449396f33a0e
Toshitaka Adachi <adatch.t@gmail.com>
2013-10-21 13:12:13 +0900

    配置換え

--- src/xlsx2vtb.py
+++ src/xlsx2vtb.py
@@ -1,13 +1,6 @@
 # coding: utf-8
 import sys
 
-class Datum(object):
-    def __init__(self, pos, fmt, typ, value):
-        self.pos = pos
-        self.fmt = int(fmt) if fmt else fmt
-        self.typ = typ
-        self.value = value
-
 
 import csv
 import os
@@ -99,6 +92,14 @@ class VTB(object):
 
     def pprint(self):
         self.write(sys.stdout)
+
+
+class Datum(object):
+    def __init__(self, pos, fmt, typ, value):
+        self.pos = pos
+        self.fmt = int(fmt) if fmt else fmt
+        self.typ = typ
+        self.value = value
 
 
 class Row(object):
